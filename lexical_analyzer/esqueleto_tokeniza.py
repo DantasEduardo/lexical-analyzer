1d23ebe91b0b8ae0837b24838513138589427e65
Eduardo Dantas de Oliveira <eduardo.oliveira@yhub.com.br>
2023-05-15 23:41:58 -0300

    add files templete

---
+++ lexical_analyzer/esqueleto_tokeniza.py
@@ -0,0 +1,67 @@
+
+# Constantes
+TESTE   = False
+
+# caracteres usados em operadores
+OPERADORES = "%*/+-!^="
+
+# caracteres usados em números inteiros
+DIGITOS = "0123456789"
+
+# ponto decimal
+PONTO = "."
+
+# todos os caracteres usados em um números float
+FLOATS = DIGITOS + PONTO
+
+# caracteres usados em nomes de variáveis
+LETRAS  = "_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# abre e fecha parenteses
+ABRE_FECHA_PARENTESES = "()"
+
+# categorias
+OPERADOR   = 1 # para operadores aritméticos e atribuição
+NUMERO     = 2 # para números: todos são considerados float
+VARIAVEL   = 3 # para variáveis
+PARENTESES = 4 # para '(' e ')
+
+# Whitespace characters: space, newline, horizontal tab,
+# vertical tab, form feed, carriage return
+BRANCOS    = [' ', '\n', '\t', '\v', '\f', '\r']
+
+# caractere que indica comentário
+COMENTARIO = "#"
+
+
+#------------------------------------------------------------
+def tokeniza(exp):
+    """(str) -> list
+
+    Recebe uma string exp representando uma expressão e cria 
+    e retorna uma lista com os itens léxicos que formam a
+    expressão.
+
+    Cada item léxico (= token) é da forma
+       
+        [item, tipo]
+
+    O componente item de um token é 
+
+        - um float: no caso do item ser um número; ou 
+        - um string no caso do item ser um operador ou 
+             uma variável ou um abre/fecha parenteses.
+
+    O componente tipo de um token indica a sua categoria
+    (ver definição de constantes acima). 
+
+        - OPERADOR;
+        - NUMERO; 
+        - VARIAVEL; ou 
+        - PARENTESES
+
+    A funçao ignora tuo que esta na exp apos o caractere
+    COMENTARIO (= "#").
+    """
+    # escreva o seu código abaixo
+    
---
+++ lexical_analyzer/main.py
@@ -0,0 +1,55 @@
+'''
+   NÃO MODIFIQUE ESTE ARQUIVO - autor MAC0122
+
+   Este arquivo contem o programa principal do projeto.
+'''
+
+# tk.tokeniza(), 
+import tokeniza as tk
+
+# categorias e dicionario "categoria: decrição" 
+import operadores as op
+
+PROMPT = "expressão >>> "
+QUIT   = ''
+
+#------------------------------------------------------------
+def main():
+    '''None -> None
+
+    Programa que lê do teclado uma expressão aritmética 
+    e imprime cada item léxico na expressão.
+
+    Exemplos:
+
+    
+    '''
+    print("Entre como uma expressão ou tecle apenas ENTER para encerrar.") 
+    expressao = input(PROMPT)
+    while expressao != QUIT:
+        lista_tokens = tk.tokeniza(expressao)
+        for token in lista_tokens:
+            # pegue item e tipo
+            item, tipo = token
+
+            # cri string com a descriçao
+            if tipo in [tk.OPERADOR, tk.PARENTESES]:
+                descricao = "'%s' : %s" %(item,op.DESCRICAO[item])
+            elif tipo == tk.VARIAVEL:
+                descricao = "'%s' : nome de variável" %item
+            elif tipo == tk.NUMERO:
+                descricao = "%f : constante float" %item
+            else:
+                descricao = "'%s' : categoria desconhecida" %item
+
+            # imprima a descriçao
+            print(descricao)
+
+        # leia próxima expressão    
+        expressao = input(PROMPT)        
+
+
+#-------------------------------------------
+# início da execução do programa
+main()
+        
